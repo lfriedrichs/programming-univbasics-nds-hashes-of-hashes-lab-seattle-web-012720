972a7002becec20aedb9c240ce00a5ddcd05a0c7
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-01-15 02:47:47 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -4,14 +4,14 @@
 #
 # The tests will guide your construction.
 
- PORTION_1 = {
-   label: "Kingdom",
-   sub_category: {
-     label: "Phylum",
-     sub_category: {
-       label: "Class"
-     }
-   }
+ #PORTION_1 = {
+   #label: "Kingdom",
+  # sub_category: {
+  #   label: "Phylum",
+  #   sub_category: {
+  #     label: "Class"
+  #   }
+#   }
  }
  
  PORTION_2 = {
